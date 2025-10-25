b7e96df70a1527940576febdcfc544aa32f08299
KimYjoo <pas4976@gmail.com>
2025-10-26 05:29:12 +0900

    feat: Add function to validate car Input String

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Console } from '@woowacourse/mission-utils'
 import inputCarProcess from './racingGame/inputProcessing/inputCarProcess.js';
+import validateCarInput from './racingGame/validation/validateCarInput.js';
 class App {
   async run() {
-    const inputCarString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const inputAttempt = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-
-    const carObjects = inputCarProcess(inputCarString);
+    try{
+      const inputCarString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      validateCarInput(inputCarString);
+      const inputAttempt = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+      const carObjects = inputCarProcess(inputCarString);
+    }
+    catch(e){
+      Console.print(e.message)
+      throw(e)
+    }
     
   }
 }
