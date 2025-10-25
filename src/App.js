f2b17ca150c344f2a92cc9efa3d31bb71e6feeff
KimYjoo <pas4976@gmail.com>
2025-10-25 23:38:25 +0900

    feat: Add user input functions of car and attempt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils'
+import inputCarProcess from './racingGame/inputProcessing/inputCarProcess.js';
 class App {
-  async run() {}
+  async run() {
+    const inputCarString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const inputAttempt = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    const carObjects = inputCarProcess(inputCarString);
+    Console.print(carObjects);
+  }
 }
 
 export default App;
