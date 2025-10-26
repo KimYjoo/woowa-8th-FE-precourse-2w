a9b29bd51552ddf02896a72e8bc6f12d0073fbb0
KimYjoo <pas4976@gmail.com>
2025-10-26 09:03:54 +0900

    feat: Edit game progress function
    - Replace math.random() with MissionUtils.pickNumberInRange
    in the game progress function

--- src/racingGame/gameProgress/index.js
+++ src/racingGame/gameProgress/index.js
@@ -1,10 +1,10 @@
-import { Console, Random } from "@woowacourse/mission-utils"
+import { Console, MissionUtils } from "@woowacourse/mission-utils"
 
 export default function racingGame({attemptNumber, carObjectList}){
     // Console.print('실행 결과');
     for(let i = 0; i < attemptNumber; i++){
         carObjectList.forEach((car) => {
-            if(Math.floor(Math.random() * 10) >= 4) car.progress += 1;
+            if(MissionUtils.Random.pickNumberInRange(0, 9) >= 4) car.progress += 1;
             const progressHyphens = Array.from({length:car.progress},(v, k) => ('-')).join('');
             Console.print(`${car.carName} : ${progressHyphens}`);
         });
