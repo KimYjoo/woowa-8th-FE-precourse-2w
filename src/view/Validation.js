d48f7acbd5d7b028ef77e560414a444e91dd599c
KimYjoo <pas4976@gmail.com>
2025-10-27 21:44:25 +0900

    style(validation.js): Add annotation on each validation

--- src/view/Validation.js
+++ src/view/Validation.js
@@ -3,22 +3,29 @@ import { ErrorMessage } from "../constants/message.js";
 import { GameSettings } from "../constants/gameSettings.js";
 
 export function validateCarInput(carInput) {
+  // 자동차 입력값이 없을 경우
   if (!carInput)
     throw Error(`${ErrorMessage.PREFIX} ${ErrorMessage.CAR_INPUT_NON}`);
+  // 자동차 입력값에 특수 문자가 포함됐을 경우
   if (Regex.MATCH_CAR_SPECIAL_CHARACTER.test(carInput))
     throw Error(
       `${ErrorMessage.PREFIX} ${ErrorMessage.CAR_INPUT_SPECIAL_CHARACTER}`
     );
+  // 자동차 입력값에 이름이 공백인 경우나 양쪽 끝에 쉼표가 포함됐을 경우
   if (Regex.CHECK_CAR_FORMAT.test(carInput))
     throw Error(`${ErrorMessage.PREFIX} ${ErrorMessage.CAR_INPUT_FORM}`);
 }
 
 export function validateAttemptInput(attemptInput) {
+  // 시도 입력값이 없을 경우
   if (!attemptInput)
     throw Error(`${ErrorMessage.PREFIX} ${ErrorMessage.ATTEMPT_INPUT_NON}`);
+
   const numberAttempt = Number(attemptInput);
+  // 시도 입력값이 숫자가 아닌 경우
   if (!isFinite(numberAttempt))
     throw Error(`${ErrorMessage.PREFIX} ${ErrorMessage.ATTEMPT_INPUT_NAN}`);
+  // 시도 입력값이 정해진 범위를 넘은 경우
   if (
     numberAttempt < GameSettings.MIN_ATTEMPT ||
     numberAttempt > GameSettings.MAX_ATTEMPT
