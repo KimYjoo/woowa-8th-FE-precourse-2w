a4232a6997ddc953c85d2ab1101ff72046acbb17
KimYjoo <pas4976@gmail.com>
2025-10-27 23:43:49 +0900

    refactor: Reduce indentation by function extraction

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import RacingGame from "./domain/RacingGame.js";
 class App {
   async run() {
     try {
-      const carList = await InputView.readCarNames();
-      const attemptCount = await InputView.readAttemptCount();
-
-      const raceGame = new RacingGame(carList);
-
-      OutputView.printRaceHeader();
-      for (let i = 0; i < attemptCount; i++) {
-        raceGame.runSingleAttempt();
-        OutputView.printSingleAttemptResult(raceGame.getCarStatusList());
-      }
-      const raceWinner = raceGame.judgeWinner();
-
-      OutputView.printWinners(raceWinner);
+      await this.#playSingleGame();
     } catch (error) {
       OutputView.printErrorMessage(error);
       throw error;
     }
+  }
+  async #playSingleGame() {
+    const carList = await InputView.readCarNames();
+    const attemptCount = await InputView.readAttemptCount();
+
+    const raceGame = new RacingGame(carList);
+
+    OutputView.printRaceHeader();
+    for (let i = 0; i < attemptCount; i++) {
+      raceGame.runSingleAttempt();
+      OutputView.printSingleAttemptResult(raceGame.getCarStatusList());
+    }
+    const raceWinner = raceGame.judgeWinner();
+
+    OutputView.printWinners(raceWinner);
   }
 }
 
